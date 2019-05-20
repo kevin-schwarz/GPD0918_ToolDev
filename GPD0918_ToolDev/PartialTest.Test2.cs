7457a318972c0cab468bcb8cd54774c879cad240
Kevin Schwarz <k.schwarz@sae.edu>
2019-05-20 14:45:38 +0200

    Unterricht 20.05.19

---
+++ GPD0918_ToolDev/PartialTest.Test2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPD0918_ToolDev
+{
+    public partial class PartialTest
+    {
+
+        public void Test2()
+        {
+            Test();
+        }
+
+    }
+}
